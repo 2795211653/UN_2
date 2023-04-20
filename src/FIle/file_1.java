137ec2162b7037276131a0cc541e98e890042d39
Chris <2795211653@qq.com>
2023-04-20 14:35:59 +0800

    file_1文件的更新

--- src/FIle/file_1.java
+++ src/FIle/file_1.java
@@ -1,7 +1,29 @@
 package FIle;
 
+import java.io.File;
+
 public class file_1 {
     public static void main(String[] args) {
+        //public File(String pathname)          根据文件路径创建文件对象
+        //public File(String parent,String chile)    根据父路径字符串和子路径字符串创建文件对象
+        //public File(File parent,String child)      根据父路径对应的文件对象和子路径对应的字符串创建文件对象
+
+        //C:\Users\Chris\Desktop
+        String str="C:\\Users\\Chris\\Desktop\\a.txt";
+        File file=new File(str);
+        System.out.println(file);
+
+        //父路径：C:\Users\Chris\Desktop
+        //子路径：a.txt
+        String parent="C:\\Users\\Chris\\Desktop";
+        String child="a.txt";
+        File f2=new File(parent,child);
+        System.out.println(f2);
+
+
+
+
+
 
     }
 }
